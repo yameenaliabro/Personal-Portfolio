9249b87325378c2b530e715b94b34b1d287a6d98
muhammadYameen <muhammadyameen5432@gmail.com>
2024-02-24 20:07:07 +0500

    feat: create a simple personal portofolio

---
+++ src/app/components/HeroSection.jsx
@@ -0,0 +1,102 @@
+/* eslint-disable react/no-unescaped-entities */
+"use client";
+import React from "react";
+import Image from "next/image";
+import { TypeAnimation } from "react-type-animation";
+import { motion } from "framer-motion";
+import Link from "next/link";
+
+const HeroSection = () => {
+
+  const downloadCV = () => {
+    // Replace '/cv/resume.pdf' with the actual path to your CV file
+    const filePath = '/cv/resume.pdf';
+
+    fetch(filePath)
+      .then(response => response.blob())
+      .then(blob => {
+        const blobUrl = URL.createObjectURL(blob);
+        const link = document.createElement('a');
+        link.href = blobUrl;
+        link.download = 'muhammadyameenresume.pdf';
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+        URL.revokeObjectURL(blobUrl);
+      })
+      .catch(error => console.error('Error fetching the file:', error));
+  };
+
+  return (
+    <section className="lg:py-16">
+      <div className="grid grid-cols-1 sm:grid-cols-12">
+        <motion.div
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5 }}
+          className="col-span-8 place-self-center text-center sm:text-left justify-self-start"
+        >
+          <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold">
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-primary-400 to-secondary-600">
+              Hello, I&apos;m{" "}
+            </span>
+            <br></br>
+            <TypeAnimation
+              sequence={[
+                "Yameen Ali",
+                1000,
+                "Web Developer",
+                1000,
+                "Mobile Developer",
+                1000,
+                "UI/UX Designer",
+                1000,
+              ]}
+              wrapper="span"
+              speed={50}
+              repeat={Infinity}
+            />
+          </h1>
+          <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
+            As a dedicated MERN stack developer with 1 year of experience, I specialize in building dynamic web applications. Proficient in React.js for creating engaging user interfaces, Node.js and Express.js for server-side development, and MongoDB for efficient database solutions. I've successfully contributed to projects, demonstrating a commitment to delivering quality code. Eager to continue learning and contribute to innovative tech projects. Let's connect and explore opportunities to collaborate! 💻🌐 #MERNDeveloper #WebDev #TechEnthusiast
+          </p>
+          <div>
+            <Link
+              href="/#contact"
+              className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-200 text-white"
+            >
+              Hire Me
+            </Link>
+            <button
+              href="/"
+              className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
+              onClick={downloadCV}
+            >
+              <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
+                Download CV
+              </span>
+            </button>
+          </div>
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5 }}
+          className="col-span-4 place-self-center mt-1 lg:mt-0"
+        >
+          <div className="rounded-3xl bg-[#181818] w-[250px] h-[250px] lg:w-[340px] lg:h-[340px] relative">
+            <Image
+              src="/images/hero-image.jpg"
+              alt="hero image"
+              className="rounded-3xl absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+              width={300}
+              height={300}
+            />
+          </div>
+        </motion.div>
+      </div>
+    </section>
+  );
+};
+
+export default HeroSection;
